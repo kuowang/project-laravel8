2a2dd925ba2ad8de04cf24b0d000deb66d11f3c9
wangkuo <everup@163.com>
2021-02-25 18:26:04 +0800

    文件导出功能修复

---
+++ config/pdf.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	'mode'                  => 'utf-8',
+	'format'                => 'A4',
+	'author'                => 'wangkuo',
+	'creator'               => 'Laravel Pdf',
+	'display_mode'          => 'fullpage',
+	'tempDir'               => base_path('../temp/'),
+    'subject'          => 'This Document will explain the whole universe.',
+    'keywords'         => 'PDF, Laravel, Package, Peace', // Separate values with comma
+];
